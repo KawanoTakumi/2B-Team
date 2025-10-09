6f14b23f878b14e7f48c01453220391fa1afabe2
MSI\GA2B <h1202416002@hiratagakuen.onmicrosoft.com>
2025-10-09 13:21:26 +0900

    何かのコミット

--- KitakuRPG/Source/KitakuRPG/KitakuRPG.h
+++ KitakuRPG/Source/KitakuRPG/KitakuRPG.h
@@ -4,3 +4,6 @@
 
 #include "CoreMinimal.h"
 
+UPROPERTY(EditAnywhere)
+USceneComponent* OurVisibleComponent;
+
---
+++ KitakuRPG/Source/KitakuRPG/PlayerPawn.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "HowTo_PlayerInput.h"
+#include "PlayerPawn.h"
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	//UCameraComponent* OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
+
+	//OurCamera->SetupAttachment(RootComponent);
+	//OurCamera->SetRelativeLocation(FVector(-250.0f, 0.0f, 250.0f));
+	//OurCamera->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+	//OurVisibleComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
