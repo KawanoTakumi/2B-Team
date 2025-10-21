89eeb5aaa4dcd34c496673d1423f4fcf010f2413
MSI\GA2B <h1202416002@hiratagakuen.onmicrosoft.com>
2025-10-21 17:21:46 +0900

    no message

---
+++ KitakuRPG/Source/KitakuRPG/CharacterStatusData.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterStatusData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KITAKURPG_API UCharacterStatusData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+
+	//Å‘å‘Ì—Í
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "STATUS")
+	int Character_MAX_HP = 0;
+	//UŒ‚—Í
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "STATUS")
+	int Character_Attack = 0;
+	//ˆÚ“®‘¬“x
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "STATUS")
+	float Character_Move_Speed = 1.0f;
+	
+};
---
+++ KitakuRPG/Source/KitakuRPG/StatusComponent.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StatusComponent.h"
+
+// Sets default values for this component's properties
+UStatusComponent::UStatusComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+
+	// ...
+}
+
+
+// Called when the game starts
+void UStatusComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+	if (StatusData)
+	{
+		//キャラクターのステータスデータを適用
+		Read_MAX_HP = StatusData->Character_MAX_HP;
+		Read_Attck = StatusData->Character_Attack;
+		Read_Speed = StatusData->Character_Move_Speed;
+
+	}
+
+}
+
+
+// Called every frame
+void UStatusComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
